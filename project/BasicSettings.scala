eceacb4da62431b2f290d04275ab051e4a237fe8
brightgenerous <katou.akihiro@gmail.com>
2016-08-12 11:13:31 +0900

    [WIP] multiple sbt

---
+++ project/BasicSettings.scala
@@ -0,0 +1,42 @@
+import play.sbt.PlayImport.PlayKeys
+import sbt.Keys._
+import sbt.{Attributed, Build}
+import sbtassembly.AssemblyKeys.assembly
+
+object BasicSettings extends Build {
+
+  //  scalaVersion : = defaultScalaVersion
+  val defaultScalaVersion = "2.11.8"
+
+  lazy val commonSettings = Seq(
+    organization := "cgmlms",
+    scalaVersion := defaultScalaVersion,
+    retrieveManaged := true
+  )
+
+  lazy val cgmlmsCommonSettings = Seq(
+    name := "cgmlms-common",
+    fullClasspath in assembly += Attributed.blank(PlayKeys.playPackageAssets.value),
+    version := "1.0-SNAPSHOT",
+    scalaVersion := defaultScalaVersion
+  )
+
+  lazy val cgmlmsInfrastructureSettings = Seq(
+    name := "cgmlms-infrastructure",
+    fullClasspath in assembly += Attributed.blank(PlayKeys.playPackageAssets.value),
+    version := "1.0-SNAPSHOT",
+    scalaVersion := defaultScalaVersion
+  )
+
+  lazy val cgmlmsDomainSettings = Seq(
+    name := "cgmlms-domain",
+    fullClasspath in assembly += Attributed.blank(PlayKeys.playPackageAssets.value),
+    version := "1.0-SNAPSHOT",
+    scalaVersion := defaultScalaVersion
+  )
+
+  lazy val cgmlmsWebSettings = Seq(
+    name := "cgmlms-web",
+    version := "1.0-SNAPSHOT"
+  )
+}
